457a3afa3e0972f70688a959ec7038e8235b8d3d
Guilherme Prates <guilherme_prates@icloud.com>
2018-05-21 12:01:11 -0300

    Fix script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt-get update 
-sudo apt-get upgrade
 
 sudo apt-get install -y build-essential
 sudo apt-get install nvidia-390
 sudo apt-get install cuda-nvidia-toolkit
 
-# CUDA 8.0
+# CUDA
 
 curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-8-0_8.0.61-1_amd64.deb
 
@@ -16,11 +16,11 @@ sudo apt-get update
 sudo apt-get install -y cuda
 
 echo "export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=/usr/local/cuda-8./lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" >> ~/.bashrc
 
 source ~/.bashrc
 
-# Download CUDNN first https://developer.nvidia.com/rdp/cudnn-download
+# Download CUDNN at https://developer.nvidia.com/rdp/cudnn-download
 
 tar xvzf cudnn-8.0-linux-x64-v5.1.tgz 
 sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
